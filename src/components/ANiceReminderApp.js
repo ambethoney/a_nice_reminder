ab22a6f3a93ca5bb89e6005821358d2d922955da
Angelina Bethoney <ambethoney@yahoo.com>
2015-07-22 18:13:48 -0400

    ajax config

--- src/components/ANiceReminderApp.js
+++ src/components/ANiceReminderApp.js
@@ -1,6 +1,8 @@
 'use strict';
 
-var React = require('react/addons');
+var React = require('react/addons'),
+    $ = require('jquery');
+
 
 // CSS
 require('normalize.css');
@@ -8,12 +10,45 @@ require('../styles/main.css');
 
 // var imageURL = require('../images/yeoman.png');
 
+
+
 var ANiceReminderApp = React.createClass({
+  getInitialState: function(){
+    return {
+      quotes: []
+    };
+  },
+  componentDidMount: function(){
+    $.ajax({
+      headers: { 'X-Mashape-Key': '5QMMdTLEcLmshgQcIECzXr43EtlGp1ch1jpjsnHebykNW2EEix'},
+      url: 'https://healthruwords.p.mashape.com/v1/quotes/',
+      type: 'GET',
+      dataType: 'JSON',
+      success: function(data){
+        var quote = data[0].media;
+        if(this.isMounted()){
+          this.setState({
+            quotes: quote
+          });
+        }
+      }
+    });
+  },
+  add: function(data) {
+      var arr = this.state.quotes;
+      arr.push({
+          quote: data
+      });
+      this.setState({quotes: arr});
+  },
   render: function() {
     return (
-      <div className="main">
-
+      <div className="container">
+      hello world
+        <img src={this.state.quotes}/>
+        <button>Need more inspiration?</button>
       </div>
+
     );
   }
 });
